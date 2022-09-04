0acb6ab8e6f71e72f7bee660ecdbbad81cd7c68f
Mike <90835468+justmike1@users.noreply.github.com>
2022-09-04 10:28:10 +0300

    Create metrics-server.tf

---
+++ EKS/metrics-server.tf
@@ -0,0 +1,17 @@
+resource "helm_release" "metrics" {
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  name       = "metrics-server"
+  version    = "6.0.12"
+  repository = "https://charts.bitnami.com/bitnami"
+
+  values = [<<EOF
+extraArgs:
+  kubelet-insecure-tls: true
+  kubelet-preferred-address-types: InternalIP
+apiService:
+  create: true
+EOF
+  ]
+  depends_on = [module.eks]
+}
