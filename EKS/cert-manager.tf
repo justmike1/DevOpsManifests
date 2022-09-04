aaf06cabd2df107bab42e07a1095865bfb66b578
Mike <90835468+justmike1@users.noreply.github.com>
2022-09-04 10:24:31 +0300

    Create cert-manager.tf

---
+++ EKS/cert-manager.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_namespace" "cm" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cm" {
+  count            = var.actions_runner_controller
+  name             = "cm"
+  namespace        = kubernetes_namespace.cm.metadata[0].name
+  create_namespace = true
+  chart            = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  version          = "v1.9.1"
+  values = [<<EOF
+    global:
+      podSecurityPolicy:
+        enabled: true
+        useAppArmor: true
+    prometheus:
+      enabled: false
+    installCRDs: true
+  EOF
+  ]
+}
