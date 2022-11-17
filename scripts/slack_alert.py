c722ffdd527e7669b417f2e4db3db3fa761516d4
Mike <90835468+justmike1@users.noreply.github.com>
2022-11-18 01:42:31 +0200

    added for loop

--- scripts/slack_alert.py
+++ scripts/slack_alert.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dataclasses import dataclass
 
 import requests as rq
@@ -7,32 +8,33 @@ import requests as rq
 @dataclass
 class AlertData:
     """
-    Fetches env variables from github actions file: build-ci.yaml
+    Fetches env variables from github actions file
     Dataclass for variables to be kept immutable on run
     """
 
     author: str = os.getenv("AUTHOR")
     event: str = os.getenv("EVENT")
-    namespace: str = os.getenv("NAMESPACE")
+    image: str = os.getenv("IMAGE")
     tag: str = os.getenv("TAG")
     webhook: str = os.getenv("SLACK_WEBHOOK")
+    developers: str = os.getenv("DEVELOPERS")
 
     def post_alert(self) -> str:
-        developers: dict = {
-            "mikejoseph": os.getenv("SLACK_M"),
-        }
+        # should be like {"mikejoseph-ah": "U021H4WSYTZ"} in DEVELOPERS secret
+        parsed_devs: dict = {dev:id for dev,id in dict(json.loads(self.developers)).items()}
         headers = {
             "Content-type": "application/json",
         }
         json_data = {
-            "text": f"CI Finished for {self.event}: {self.namespace}.{self.tag}, Author: <@{developers[self.author]}>",
+            "text": f"Build & Push finished successfully for {self.image}:{self.tag}, Event: {self.event}, Author: <@{parsed_devs[self.author]}>",
         }
         res = rq.post(
             url=f"https://hooks.slack.com/services/{self.webhook}",
             headers=headers,
             json=json_data,
         )
-        print(res.status_code)
+        print(json_data)
+        print(res, res.status_code)
 
 
 if __name__ == "__main__":
