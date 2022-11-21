3bf9de0e880d38f9f075060c6f5147c92401a4da
Mike Joseph <mikejoseph@Mikes-MacBook-Pro.local>
2022-11-21 11:16:06 +0200

    fixed tavisod didn't return value

--- tools/tavisod/tavisod/get_secrets.py
+++ tools/tavisod/tavisod/get_secrets.py
@@ -9,6 +9,19 @@ class getSecret(object):
     # Create the Secret Manager client.
     client = secretmanager.SecretManagerServiceClient()
 
+    @staticmethod
+    def verifyData(response):
+        # Verify payload checksum.
+        crc32c = Checksum()
+        crc32c.update(response.payload.data)
+        if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+            print("Data corruption detected.")
+    
+    @staticmethod
+    def parseData(response):
+        payload = response.payload.data.decode("UTF-8")
+        return payload
+
     def fromGsm(self, secret_id) -> str:
         if os.getenv(secret_id):
             return os.getenv(secret_id)
@@ -18,16 +31,5 @@ class getSecret(object):
         response = self.client.access_secret_version(name=name)
         # Verify Checksum
         self.verifyData(response)
-        # return response
-        self.parseRes(response)
-
-    def verifyData(self, response):
-        # Verify payload checksum.
-        crc32c = Checksum()
-        crc32c.update(response.payload.data)
-        if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-            print("Data corruption detected.")
-    
-    def parseRes(self, response) -> str:
-        payload = response.payload.data.decode("UTF-8")
-        return payload
+        # return parsed response
+        return self.parseData(response)
